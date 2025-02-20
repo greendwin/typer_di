2f6c6ac35931454bdca4db6e5c264243a4bbff2f
Evgeniy A. Cymbalyuk <cimbaluk@gmail.com>
2025-02-20 10:52:18 +0300

    `mypy`: use strict typing

--- src/typer_di/compat.py
+++ src/typer_di/compat.py
@@ -1,31 +1,36 @@
 import inspect
 import sys
+from typing import Any, Callable
 
 if sys.version_info >= (3, 9):
-    from typing import Annotated
+    from typing import Annotated as Annotated
 else:  # pragma: no cover
-    from typing_extensions import Annotated
+    from typing_extensions import Annotated as Annotated
 
 if sys.version_info >= (3, 10):
-    from typing import TypeAlias
+    from typing import TypeAlias as TypeAlias
 else:  # pragma: no cover
-    from typing import Any as TypeAlias
+    from typing_extensions import TypeAlias as TypeAlias
 
 
 if sys.version_info >= (3, 10):
 
-    def signature(func, eval_str: bool = False):
+    def signature(
+        func: Callable[..., Any], eval_str: bool = False
+    ) -> inspect.Signature:
         return inspect.signature(func, eval_str=eval_str)
 
 else:  # pragma: no cover
     # just don't eval annotations on lower versions
-    def signature(func, eval_str: bool = False):
+    def signature(
+        func: Callable[..., Any], eval_str: bool = False
+    ) -> inspect.Signature:
         sig = inspect.signature(func)
 
         if not eval_str:
             return sig
 
-        def eval_if_str(obj):
+        def eval_if_str(obj: Any) -> Any:
             if not isinstance(obj, str):
                 return obj
             return eval(obj, sys.modules[func.__module__].__dict__)
